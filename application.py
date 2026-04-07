a9f8feff6bc5c6eeb3c857d842978768b4008647
Anushka3108 <rajpootanushka4499@gmail.com>
2026-04-07 10:46:31 +0530

    fixed issues in index and home file

--- application.py
+++ application.py
@@ -20,17 +20,14 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        reading = float(request.form.get('reading_score'))
-        writing = float(request.form.get('writing_score'))
-        
         data=CustomData(
             gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('race_ethnicity'),
+            race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=reading,
-            writing_score=writing
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
@@ -40,9 +37,8 @@ def predict_datapoint():
         predict_pipeline=PredictPipeline()
         print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
-        result = float(round(results[0], 2))
         print("after Prediction")
-        return render_template('home.html',results=result, values=[reading, writing])
+        return render_template('home.html',results=prediction)
     
 
 if __name__ == "__main__":
